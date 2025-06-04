db6c3df750d51f74a9b8ba3e1767047f4d086496
Thaddeus <thaddeuspearson@gmail.com>
2025-06-04 08:08:01 +0200

    add python solutions to validate_subsequence

--- Arrays/Easy/validate_subsequence/solutions.py
+++ Arrays/Easy/validate_subsequence/solutions.py
@@ -1,2 +1,36 @@
-def solution_1():
-    pass
+def solution_1(array: list, sequence: list) -> bool:
+    """
+    Time Complexity: O(n) - n is the length of array
+    Space Complexity: O(1)
+    """
+    arr_idx = 0
+    sub_seq_idx = 0
+    arr_len = len(array)
+    sub_seq_len = len(sequence)
+
+    while arr_idx < arr_len and sub_seq_idx < sub_seq_len:
+
+        if array[arr_idx] == sequence[sub_seq_idx]:
+            sub_seq_idx += 1
+
+        arr_idx += 1
+
+    return arr_idx == arr_len
+
+
+def solution_2(array: list, sequence: list) -> bool:
+    """
+    Time Complexity: O(n) - n is the length of array
+    Space Complexity: O(1)
+    """
+    sub_seq_idx = 0
+    seq_len = len(sequence)
+
+    for elem in array:
+        if sub_seq_idx == seq_len:
+            break
+
+        if elem == sequence[sub_seq_idx]:
+            sub_seq_idx += 1
+
+    return sub_seq_idx == seq_len
