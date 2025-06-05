3e3ffa3af63ac5afc2928ee772a23e3f26e4e2c5
Thaddeus <thaddeuspearson@gmail.com>
2025-06-05 03:14:27 +0200

    add sorted_squared_array challenge

---
+++ Arrays/Easy/sorted_squared_array/description.md
@@ -0,0 +1,17 @@
+# Sorted Squared Array
+
+## Description
+
+Given a **non-empty** `array` of integers sorted in **non-decreasing order**, create a new array of the same length containing the **squares of each number**, also sorted in **non-decreasing order**.
+
+Since the original array can contain both negative and positive numbers, squaring the elements might change their order. Your task is to correctly compute the squares and return them in sorted order.
+
+## Example
+
+```
+Input:
+    array = [-7, -3, 1, 9, 22, 30]
+
+Output:
+    [1, 9, 49, 81, 484, 900]
+```
---
+++ Arrays/Easy/sorted_squared_array/solutions.py
@@ -0,0 +1,24 @@
+import sys
+from pathlib import Path
+utils_path = Path(__file__).resolve().parents[3] / "utils"
+sys.path.append(str(utils_path))
+from run_tests import run_tests
+from inspect import getmembers, isfunction
+
+
+def solution_1(array: list) -> list:
+    """
+    Time Complexity: O()
+    Space Complexity: O()
+    """
+    pass
+
+
+if __name__ == "__main__":
+    solutions = [
+        f for _,
+        f in getmembers(sys.modules[__name__], isfunction)
+        if "solution" in f.__name__
+    ]
+    for func in solutions:
+        run_tests(func, "tests.json")
