1a83fdee2756fdfbef2bac6f4fa9bcc65f371eb9
Thaddeus <thaddeuspearson@gmail.com>
2025-06-07 22:40:57 +0200

    rf: solution_1 sorted_squared_array

--- Arrays/Easy/sorted_squared_array/solutions.py
+++ Arrays/Easy/sorted_squared_array/solutions.py
@@ -11,30 +11,30 @@ def solution_1(array: list) -> list:
     Time Complexity: O(n) - n is the size of the input array
     Space Complexity: O(n)
     """
-    l = 0
-    r = 0
+    l_idx = 0
+    r_idx = 0
     array_len = len(array)
     sorted_squared = []
 
-    while r < array_len and array[r] < 0:
-        r += 1
-    l = r - 1
+    while r_idx < array_len and array[r_idx] < 0:
+        r_idx += 1
+    l_idx = r_idx - 1
 
-    while 0 <= l and r < array_len:
+    while 0 <= l_idx and r_idx < array_len:
 
-        if array[r] <= abs(array[l]):
-            sorted_squared.append(array[r]**2)
-            r += 1
+        if array[r_idx] <= abs(array[l_idx]):
+            sorted_squared.append(array[r_idx]**2)
+            r_idx += 1
         else:
-            sorted_squared.append(array[l]**2)
-            l -= 1
+            sorted_squared.append(array[l_idx]**2)
+            l_idx -= 1
 
-    if l < 0:
-        for r in range(r, array_len):
-            sorted_squared.append(array[r]**2)
+    if l_idx < 0:
+        for r_idx in range(r_idx, array_len):
+            sorted_squared.append(array[r_idx]**2)
     else:
-        for l in range(l, -1, -1):
-            sorted_squared.append(array[l]**2)
+        for l_idx in range(l_idx, -1, -1):
+            sorted_squared.append(array[l_idx]**2)
 
     return sorted_squared
 
