85df4df03d2f1a484e35424ef2b40e0b4c583d4a
Thaddeus <thaddeuspearson@gmail.com>
2025-06-17 14:31:37 +0200

    add reverse_word_pairs solution

--- Strings/Easy/reverse_word_pairs/solutions.py
+++ Strings/Easy/reverse_word_pairs/solutions.py
@@ -4,12 +4,22 @@ path.append(str(Path(__file__).resolve().parents[3]))
 from utils.run_tests import run_tests  # noqa E402
 
 
-def solution_1():
+def solution_1(words):
     """
-    Time Complexity: O()
-    Space Complexity: O()
+    Time Complexity: O(n) - n is the length of words
+    Space Complexity: O(n)
     """
-    pass
+    seen = set()
+    reverse_word_pairs = []
+
+    for word in words:
+        if word not in seen:
+            reversed_word = word[::-1]
+            if reversed_word in seen:
+                reverse_word_pairs.append([reversed_word, word])
+            else:
+                seen.add(word)
+    return reverse_word_pairs
 
 
 if __name__ == "__main__":
