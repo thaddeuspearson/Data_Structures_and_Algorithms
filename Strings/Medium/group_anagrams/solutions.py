9865ac74063cebb76213d6b317fb209db26a8d3f
Thaddeus <thaddeuspearson@gmail.com>
2025-06-19 23:39:50 +0200

    add group_anagrams challenge to Strings

--- Strings/Medium/group_anagrams/solutions.py
+++ Strings/Medium/group_anagrams/solutions.py
@@ -4,12 +4,19 @@ path.append(str(Path(__file__).resolve().parents[3]))
 from utils.run_tests import run_tests  # noqa E402
 
 
-def solution_1():
+def solution_1(strings):
     """
-    Time Complexity: O()
-    Space Complexity: O()
+    Time Complexity: O(n * m * logm) - n is length of strings,
+                                       m is length of longest word
+    Space Complexity: O(n * m)
     """
-    pass
+    lookup = {}
+
+    for string in strings:
+        curr = tuple(sorted(string))
+        lookup.setdefault(curr, [])
+        lookup[curr].append(string)
+    return list(lookup.values())
 
 
 if __name__ == "__main__":
