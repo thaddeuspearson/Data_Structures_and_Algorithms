113fef2ec3091b869435e67a310f1e0583b136bd
Thaddeus <thaddeuspearson@gmail.com>
2025-06-28 07:50:57 +0200

    add solutions to

---
+++ Searching/Easy/binary_search/description.md
@@ -0,0 +1,26 @@
+# Binary Search
+
+## Description
+
+Given a sorted `array` of integers and a `target` value, implement a function that uses the **binary search algorithm** to determine the index of the target within the array.  
+
+If the target value is found, return its index. If it does not exist in the array, return `-1`.
+
+## Example
+
+```
+Input:
+    array = [-10, -3, 0, 5, 9, 12]
+    target = 9
+
+Output:
+    4
+------------------------------------
+
+Input:
+    array = [1, 3, 5, 7, 9, 11, 13]
+    target = 6
+
+Output:
+    -1
+```
---
+++ Searching/Easy/binary_search/solutions.py
@@ -0,0 +1,53 @@
+from sys import path
+from pathlib import Path
+path.append(str(Path(__file__).resolve().parents[3]))
+from utils.run_tests import run_tests  # noqa E402
+
+
+def solution_1(array, target):
+    """
+    Time Compelxity: O(log n) - n is the length of array
+    Space complexity: O(1)
+    """
+    return recursive_helper(array, target, 0, len(array))
+
+
+def recursive_helper(array, target, left, right):
+    mid = (left + right) // 2
+
+    if left >= right:
+        return -1
+    elif array[mid] == target:
+        return mid
+    else:
+        if array[mid] <= target:
+            left = mid+1
+        else:
+            right = mid
+    return recursive_helper(array, target, left, right)
+
+
+def solution_2(array, target):
+    """
+    Time Compelxity: O(log n) - n is the length of array
+    Space complexity: O(1)
+    """
+    left = 0
+    right = len(array)
+
+    while left < right:
+        mid = (left + right) // 2
+        print(f"{left=} {right=} {mid=} {target=}")
+
+        if array[mid] == target:
+            return mid
+        else:
+            if array[mid] <= target:
+                left = mid+1
+            else:
+                right = mid
+    return -1
+
+
+if __name__ == "__main__":
+    run_tests()
